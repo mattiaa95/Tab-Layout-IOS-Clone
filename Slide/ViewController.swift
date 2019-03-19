b9775e009434dec122a44c2bdcade1fe38132b42
Mattia <mattia@mac-movilidad.local>
2019-03-19 11:05:59 +0000

    Initial Commit

---
+++ Slide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slide
+//
+//  Created by Mattia on 19/03/2019.
+//  Copyright © 2019 Mattia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
